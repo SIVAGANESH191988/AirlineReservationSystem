025b51d427bf072543a1745eef69c01ffc417b05
SivaGanesh8577 <sivaganesh20002@gmail.com>
2024-08-09 11:14:15 +0530

    tt

--- Backend/AirlineReservationSystem/src/main/java/com/siva/AirlineReservationSystem/repository/UserRepository.java
+++ Backend/AirlineReservationSystem/src/main/java/com/siva/AirlineReservationSystem/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.siva.AirlineReservationSystem.repository;
 import com.siva.AirlineReservationSystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
     User findByUsername(String username);
     User findByEmail(String email);
     User findByUserId(int userID);
